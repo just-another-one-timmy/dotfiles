2e7548a3ef9c16c68485892a67fb3ba36d0a9346
Iaroslav Tymchenko <jtimchenko@gmail.com>
2013-08-04 04:08:43 +0300

    emms

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,10 +58,20 @@
 (add-to-list 'default-frame-alist '(font . "Consolas-16"))
 (set-default-font "Consolas-16")
 
-
+;;
+;; smex - ido in minibuffer
+;;
 (add-to-list 'load-path "~/.emacs.d//smex")
 (require 'smex)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 ;; This is your old M-x.
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;;
+;; emms - emacs multimedia system
+;;
+(add-to-list 'load-path "~/.emacs.d//emms/lisp")
+(require 'emms-setup)
+(emms-standard)
+(emms-default-players)
